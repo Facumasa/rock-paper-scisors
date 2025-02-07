858910a86ece670f59c8c18594df59714fda0211
Facumasa <facundomasari@gmail.com>
2025-02-07 09:39:13 +0100

    Choice from computer more clear now, play 5 games and count score

--- scipt.js
+++ scipt.js
@@ -1,8 +1,3 @@
-let humanScore=0;
-let computerScore=0;
-console.log("Human ",humanScore);
-console.log("computer",computerScore);
-
 // Function that returns random choice from computer
 function getComputerChoice () {
   return Math.floor(Math.random()*3+1)      
@@ -14,15 +9,15 @@ function getComputerChoice () {
 function getHumanChoice () {
     let sign=prompt("Rock, paper scisors on three...two...one...")
     if (sign.toLowerCase()==="rock") {
-        console.log("you have chosen rock");
+        console.log("You have chosen rock");
         return 1
     }
     else if (sign.toLowerCase()==="paper") {
-        console.log("you have chosen paper");
+        console.log("You have chosen paper");
         return 2
     } 
     else if (sign.toLowerCase()==="scisors") {
-        console.log("you have chosen scisors");
+        console.log("You have chosen scisors");
         return 3
     }
     else {
@@ -32,11 +27,32 @@ function getHumanChoice () {
 
 
 function playGame() {
-    console.log("we are gonna play to the best of 5 games")
-    for (let i = 0; i < 4; i++) {
-        console.log(i);
-        
+    console.log("we are gonna play to the best of 5 games");
+    let humanScore=0;
+    let computerScore=0;
+    for (let i = 0; i < 5; i++) {
+        console.log("");
+        console.log("Our current score is Human:",humanScore,"; Computer:",computerScore);
+        console.log("");
+        let human=getHumanChoice();
+        let computer=getComputerChoice();
+        switch (computer) {
+            case 1:
+                console.log("And I have chosen Rock");
+                break;
+            case 2:
+                console.log("And I have chosen Paper");
+                break;
+            case 3:
+                console.log("And I have chosen Scisors");
+                break;
+            default:
+                console.log("Something went WRONG");
+                break;
+        }
+        playRound(human,computer);
     }
+    console.log("Our FINAL score is Human:",humanScore,"; Computer:",computerScore);
 
     // Function that plays one round
     function playRound(humanChoice,computerChoice) {
@@ -59,18 +75,22 @@ function playGame() {
         else if (humanChoice===1) {
             if (computerChoice===2) {
                 console.log("You lose! Paper beats Rock");
+                computerScore++;
             }
             else {
                 console.log("You win! Rock beats Scisors");
+                humanScore++;
             }
         }
 
         else if (humanChoice===2) {
             if (computerChoice===1) {
                 console.log("You win! Paper beats Rock");
+                humanScore++;
             }
             else {
                 console.log("You lose! Scisors beats Paper");
+                computerScore++;
             }
             
         }
@@ -78,9 +98,11 @@ function playGame() {
         else if (humanChoice===3) {
             if (computerChoice===1) {
                 console.log("You lose! Rock beats Scisors");
+                computerScore++;
             }
             else {
                 console.log("You win! Scisors beats Paper");
+                humanScore++;
             }
             
         }
@@ -92,9 +114,3 @@ function playGame() {
 }
 
 playGame();
-
-let human=getHumanChoice();
-let computer=getComputerChoice();
-
-console.log("And I have chosen ",computer);
-playRound(human,computer);
