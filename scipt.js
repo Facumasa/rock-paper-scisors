b8f07952300972dcaa7d5c8d9d2ab3a803c93209
Facumasa <facundomasari@gmail.com>
2025-03-18 10:18:15 +0100

    I dont know what i changed now

--- scipt.js
+++ scipt.js
@@ -1,119 +1,35 @@
-// Function that returns random choice from computer
-function getComputerChoice () {
-  return Math.floor(Math.random()*3+1)      
-}
-
-// 1=rock; 2=paper; 3=scisors
-
-// Function that takes human choice
-function getHumanChoice () {
-    let sign=prompt("Rock, paper scisors on three...two...one...")
-    if (sign.toLowerCase()==="rock") {
-        console.log("You have chosen rock");
-        return 1
-    }
-    else if (sign.toLowerCase()==="paper") {
-        console.log("You have chosen paper");
-        return 2
-    } 
-    else if (sign.toLowerCase()==="scisors") {
-        console.log("You have chosen scisors");
-        return 3
-    }
-    else {
-        console.log("I think you might have mispelled your choice, plese select rock, paper or scisors");
-    }
-}    
+// Global variables  -----------------------------------------------------------
+let humanScore=0;
+let computerScore=0;
 
 
-function playGame() {
-    console.log("we are gonna play to the best of 5 games");
-    let humanScore=0;
-    let computerScore=0;
-    // for (let i = 0; i < 5; i++) {
-        console.log("");
-        console.log("Our current score is Human:",humanScore,"; Computer:",computerScore);
-        console.log("");
-        let human=getHumanChoice();
-        let computer=getComputerChoice();
-        switch (computer) {
-            case 1:
-                console.log("And I have chosen Rock");
-                break;
-            case 2:
-                console.log("And I have chosen Paper");
-                break;
-            case 3:
-                console.log("And I have chosen Scisors");
-                break;
-            default:
-                console.log("Something went WRONG");
-                break;
-        }
-        playRound(human,computer);
-    // }
-    console.log("Our FINAL score is Human:",humanScore,"; Computer:",computerScore);
 
-    // Function that plays one round
-    function playRound(humanChoice,computerChoice) {
-        // Tied
-        if (humanChoice===computerChoice) {
-            console.log("We tied, we both picked");
-            switch (humanChoice) {
-                case 1:
-                    console.log("Rock");
-                    break;
-                case 2:
-                    console.log("Paper");
-                    break;
-                default:
-                    console.log("Scisors");
-                    break;
-            }
-        }
+// DOM modification -----------------------------------------------------------
 
-        else if (humanChoice===1) {
-            if (computerChoice===2) {
-                console.log("You lose! Paper beats Rock");
-                computerScore++;
-            }
-            else {
-                console.log("You win! Rock beats Scisors");
-                humanScore++;
-            }
-        }
+const container = document.querySelector("#container");
 
-        else if (humanChoice===2) {
-            if (computerChoice===1) {
-                console.log("You win! Paper beats Rock");
-                humanScore++;
-            }
-            else {
-                console.log("You lose! Scisors beats Paper");
-                computerScore++;
-            }
-            
-        }
+const score = document.createElement("h3");
+score.classList.add("content");
 
-        else if (humanChoice===3) {
-            if (computerChoice===1) {
-                console.log("You lose! Rock beats Scisors");
-                computerScore++;
-            }
-            else {
-                console.log("You win! Scisors beats Paper");
-                humanScore++;
-            }
-            
-        }
+score.textContent = 'Right now the score is: Human '+humanScore+' and Computer '+computerScore;
 
-        else {
-            console.log("something went wrong, you picked ",humanChoice," and I picked ",computerChoice," but somwhere there was an error");
-        }
-    }
-}
+container.appendChild(score);
 
-//playGame();
+
+
+const writeHuman = document.createElement("div");
+writeHuman.classList.add("content");
+
+container.appendChild(writeHuman);
+
+
+const writeComputer = document.createElement("div");
+writeComputer.classList.add("content");
+
+container.appendChild(writeComputer);
+
+
+
 
 // creates a constant that is a node list (almost an array) that have all the buttons
 const buttons = document.querySelectorAll("button");
@@ -126,7 +42,182 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener("click", () => {
-      alert(button.id);
-      console.log(button.id);
+        playRound(getHumanChoice(button.id),getComputerChoice());
+    checkScore();
     });
   });
+
+
+
+
+
+// Functions: -----------------------------------------------------------
+
+// Function that returns random choice from computer
+function getComputerChoice () {
+  return Math.floor(Math.random()*3+1)      
+}
+
+// 1=rock; 2=paper; 3=scisors
+
+// Function that takes human choice
+
+function getHumanChoice (sign) {
+    if (sign.toLowerCase()==="rock") {
+        writeHuman.textContent = "You have chosen rock";
+        return 1
+    }
+    else if (sign.toLowerCase()==="paper") {
+        writeHuman.textContent = "You have chosen paper";
+        return 2
+    } 
+    else if (sign.toLowerCase()==="scisors") {
+        writeHuman.textContent = "You have chosen scisors";
+        return 3
+    }
+    else {
+        writeHuman.textContent = "I think there was an error";
+    }
+}    
+
+// We remore this part, which is to play five rounds
+// ----------------------------------------------------------------------
+// function playGame() {
+//     console.log("we are gonna play to the best of 5 games");
+//     let humanScore=0;
+//     let computerScore=0;
+//     // for (let i = 0; i < 5; i++) {
+//         console.log("");
+//         console.log("Our current score is Human:",humanScore,"; Computer:",computerScore);
+//         console.log("");
+//         let human=humanChoice;
+//         let computer=getComputerChoice();
+//         switch (computer) {
+//             case 1:
+//                 console.log("And I have chosen Rock");
+//                 break;
+//             case 2:
+//                 console.log("And I have chosen Paper");
+//                 break;
+//             case 3:
+//                 console.log("And I have chosen Scisors");
+//                 break;
+//             default:
+//                 console.log("Something went WRONG");
+//                 break;
+//         }
+//         playRound(human,computer);
+//     // }
+//     console.log("Our FINAL score is Human:",humanScore,"; Computer:",computerScore);
+// }
+// ----------------------------------------------------------------------
+
+
+    // Function that plays one round
+    function playRound(humanChoice,computerChoice) {
+        // Tied
+        if (humanChoice===computerChoice) {
+            writeComputer.textContent ="We tied, we both picked";
+            switch (humanChoice) {
+                case 1:
+                    writeComputer.textContent =writeComputer.textContent + " Rock";
+                    break;
+                case 2:
+                    writeComputer.textContent =writeComputer.textContent + " Paper";
+                    break;
+                default:
+                    writeComputer.textContent =writeComputer.textContent + " Scisors";
+                    break;
+            }
+        }
+
+        else if (humanChoice===1) {
+            if (computerChoice===2) {
+                writeComputer.textContent ="You lose! Paper beats Rock";
+                computerScore++;
+            }
+            else {
+                writeComputer.textContent ="You win! Rock beats Scisors";
+                humanScore++;
+            }
+        }
+
+        else if (humanChoice===2) {
+            if (computerChoice===1) {
+                writeComputer.textContent ="You win! Paper beats Rock";
+                humanScore++;
+            }
+            else {
+                writeComputer.textContent ="You lose! Scisors beats Paper";
+                computerScore++;
+            }
+            
+        }
+
+        else if (humanChoice===3) {
+            if (computerChoice===1) {
+                writeComputer.textContent ="You lose! Rock beats Scisors";
+                computerScore++;
+            }
+            else {
+                writeComputer.textContent ="You win! Scisors beats Paper";
+                humanScore++;
+            }
+            
+        }
+
+        else {
+            writeComputer.textContent ="something went wrong, you picked ",humanChoice," and I picked ",computerChoice," but somwhere there was an error";
+        }
+        score.textContent = 'Right now the score is: Human '+humanScore+' and Computer '+computerScore;
+    }
+
+
+function checkScore() {
+ if (humanScore===5) {
+
+    // Apply inline styles for `white-space: pre-line` to handle the line breaks
+    score.style.whiteSpace = "pre-line"; // Enables \n to create line breaks
+
+    score.style.color = "green";
+    score.textContent = 'You WON!!!! \n The final score was Human '+humanScore+' and Computer '+computerScore;
+    gameOver();
+}
+ else if (computerScore===5) {
+
+    // Apply inline styles for `white-space: pre-line` to handle the line breaks
+    score.style.whiteSpace = "pre-line"; // Enables \n to create line breaks
+
+    score.style.color = "red";
+    score.textContent = 'You Lost against the machine =( \n The final score was Human '+humanScore+' and Computer '+computerScore;
+    gameOver();
+}
+}
+
+function gameOver(){
+    writeComputer.textContent ='';
+    writeHuman.textContent ='';
+
+    // removes all <button> elements, even the ones created in the html:
+    document.querySelectorAll("button").forEach(button => {
+        button.remove();
+    });
+
+    // Play again? If clicked reloads the hole page (all from chat GPT)
+    const reloadButton = document.createElement("button"); // Create button
+
+    reloadButton.style.fontSize = "20px"; // Increase text size
+    reloadButton.style.padding = "15px 30px"; // Make it larger
+    reloadButton.style.width = "200px"; // Set button width
+    reloadButton.style.height = "60px"; // Set button height
+    reloadButton.style.borderRadius = "10px"; // Rounded corners
+
+    reloadButton.textContent = "Play again"; // Set button text
+    reloadButton.classList.add("reload-btn"); // Add a class (optional)
+
+    reloadButton.addEventListener("click", () => {
+        location.reload(); // Reloads the page
+    });
+
+    document.body.appendChild(reloadButton); // Add the button to the page
+}
